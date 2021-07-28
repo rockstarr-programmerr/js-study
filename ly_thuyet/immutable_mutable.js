93b858e29253c9e0c46bbec26e0f67503ba52ef1
Bunbu_HuyCV <huy.chu.van@bunbusoft.com>
2021-07-28 15:42:49 +0700

    Immutable mutable

--- ly_thuyet/immutable_mutable.js
+++ ly_thuyet/immutable_mutable.js
@@ -6,7 +6,7 @@ let person = {
     age: 23
 }
 
-let people = ['Hieu', 'Ha', 'Trung', 'co giao'];
+let people = ['Hieu', 'Ha', 'Trung', 'Co Giao'];
 
 /**
  * Output của những dòng dưới đây là gì?
@@ -15,10 +15,19 @@ let people = ['Hieu', 'Ha', 'Trung', 'co giao'];
 name.toUpperCase();
 console.log(name);
 
+// => return: 'Trung' (chỉ thực hiện toUpperCase() nhưng chưa gán lại giá trị cho biến name => vẫn lấy giá trị ban đầu)
+
 people.push('A.Linh');
 console.log(people);
+
+// return: ['Hieu', 'Ha', 'Trung', 'Co Giao', 'A.Linh']; push() => thêm phần tử vào cuối mảng, trả về một mảng mới có độ dài mới
 
 let otherPerson = person;
 otherPerson.name = 'Dien';
 console.log(person);
 console.log(otherPerson);
+
+/**
+ * return { name: 'Dien', age: 23 }
+ * person và otherPerson đều tham chiếu đến 1 value
+ */
