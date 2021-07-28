eb24ee1faa1b8b54d092499b6d85c4152f4a682b
Bunbu_HuyCV <huy.chu.van@bunbusoft.com>
2021-07-28 15:42:29 +0700

    Var, let, const javascript

---
+++ ly_thuyet/var_let_const.js
@@ -0,0 +1,21 @@
+/**
+ * - var:
+ *    + global scope
+ *    + có thể gán lại giá trị
+ *    + tạo một thuộc tính toàn cục trên window (window.variable)
+ */
+
+/**
+ * - let:
+ *    + block scope
+ *    + có thể gán lại giá trị nhưng không thể khai báo lại
+ *    + không tạo thuộc tính toàn cục trên window như var
+ */
+
+/**
+ * - const:
+ *    + block scope
+ *    + không thể khai báo & gán lại biến
+ *    + khai báo phải có 1 giá trị ban đầu
+ *    + không tạo thuộc tính toàn cục trên window như var
+ */
