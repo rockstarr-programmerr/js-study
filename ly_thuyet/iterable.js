4232a29250139841c06c4d3b391d02d6ed644c4c
Bunbu_HuyCV <huy.chu.van@bunbusoft.com>
2021-07-28 15:43:01 +0700

    Iterable

--- ly_thuyet/iterable.js
+++ ly_thuyet/iterable.js
@@ -8,13 +8,18 @@ for (const i of numbers) {
     console.log(i);
 }
 
+// 1, 2, 3, 4 (trả về value).
+
 for (const i in numbers) {
     console.log(i);
 }
+// 0, 1, 2, 3 (trả về index).
 
 for (const i of 'abcd') {
     console.log(i);
 }
+
+// a, b, c, d (trả về các chữ cái trong chuỗi).
 
 
 /**
@@ -28,17 +33,38 @@ numbers.forEach(number => {
     console.log(number);
 })
 
+// Duyện qua từng phần tử có trong mảng.
+
 const multipliedNumbers = numbers.map(number => number * 2);
 console.log(multipliedNumbers);
+
+/* Duyệt qua phần tử trong mảng, trả về một mảng với value mới. 
+=> [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+*/
 
 const bigNumbers = numbers.filter(number => number > 5);
 console.log(bigNumbers);
 
+/* Duyệt quả các phần tử của mảng, trả về một mảng mới với các value thỏa mãn điều kiện. 
+=> [6, 7, 8, 9, 10]
+*/
+
 const number8 = numbers.find(number => number === 8);
-console.log(number);
+console.log(number8);
+
+/* Trả về value thỏa mãn điều kiện
+- Nếu thỏa mãn => return value
+- Nếu không có value => return undefined
+- Nếu có 2 value trùng giá trị => return value đầu tiên
+=> 8
+*/
 
 const notExistNumber = numbers.find(number => number === 100);
 console.log(notExistNumber);
+
+/**
+ * => undefined (value không tồn tại trong mảng)
+ */
 
 
 /**
@@ -52,3 +78,9 @@ const obj = {
 for (const [key, value] of Object.entries(obj)) {
     console.log(key, value);
 }
+
+/**
+ * Object.entries => return key, value of object => array [key, value]
+ * => amazing, goodjob
+ * => coding, is fun
+ */
