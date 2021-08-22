3fc984760562cab869b713905a38ef6cacb45a27
tunv <tunguyen060811@gmail.com>
2021-08-22 18:58:52 +0700

    Do excercise about callback function

--- ly_thuyet/callback.js
+++ ly_thuyet/callback.js
@@ -13,8 +13,10 @@ function callAPI (params, callback) {
     }, 2000);
 }
 
+
 /**
  * Câu hỏi 1: Khi chạy hàm `callAPI`, điều gì sẽ xảy ra?
+ * Khi chạy hàm 'callAPI' và truyền đủ 2 tham số params, callback thì sau 2 giây hàm onFinish sẽ được chạy
  */
 
 /**
@@ -32,6 +34,35 @@ function callAPI (params, callback) {
  *      + Sau 3 giây, console hiện 'API_2 done'
  *      + Sau 6 giây, console hiện 'API_3 done'
  */
+function api_1 () {
+    console.log('Finished!!');
+    console.log('API_1 done')
+}
+
+function api_2 () {
+    console.log('Finished!!');
+    console.log('API_2 done')
+}
+
+function api_3 () {
+    console.log('Finished!!');
+    console.log('API_3 done')
+}
+
+function callAPI (callAPI1, callAPI2, callAPI3) {
+    setTimeout(function () {
+        callAPI1()
+    }, 1000);
+
+    setTimeout(function () {
+        callAPI2()
+    }, 2000);
+
+    setTimeout(function () {
+        callAPI3()
+    }, 3000);
+}
+
 
 /**
  * Câu hỏi 3: Viết code cho tình huống sau:
@@ -44,3 +75,49 @@ function callAPI (params, callback) {
  *      + Gọi API_2, log ra kết quả
  *      + Gọi API_3, log ra kết quả
  */
+
+function api_1 () {
+    let nums = [1, 2, 3, 4]
+    let randomIndex = Math.floor(Math.random() * 4)
+    let result = nums[randomIndex]
+    console.log('Result of Api 1 is: ', result)
+    console.log('------------------------------')
+    return result
+}
+
+function api_2 (num) {
+    let nums = [5, 6, 7, 8]
+    let randomIndex = Math.floor(Math.random() * 4)
+    let result = nums[randomIndex] + num
+    console.log('Result of Api 1 is: '. num)
+    console.log('Random number of Api 2 is: ', nums[randomIndex])
+    console.log('Result of Api 2 is: ', result)
+    console.log('------------------------------')
+    return result
+}
+
+function api_3 (num) {
+    let nums = [9, 10, 11, 12]
+    let randomIndex = Math.floor(Math.random() * 4)
+    let result = nums[randomIndex] + num
+    console.log('Result of Api 2 is: '. num)
+    console.log('Random number of Api 3 is: ', nums[randomIndex])
+    console.log('Result of Api 3 is: ', result)
+    console.log('------------------------------')
+    return result
+}
+
+function callAPI (callAPI1, callAPI2, callAPI3) {
+    let numberOfAPI1, numberOfAPI2
+    setTimeout(function () {
+        numberOfAPI1 = callAPI1()
+    }, 1000);
+
+    setTimeout(function () {
+        numberOfAPI2 = callAPI2(numberOfAPI1)
+    }, 2000);
+
+    setTimeout(function () {
+        callAPI3(numberOfAPI2)
+    }, 3000);
+}
